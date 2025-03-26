69f4b2ff885be49c518a059c4edf3c682dc63e30
Amiya Maity <Amiya_Maity@epam.com>
2025-03-26 14:28:23 +0530

    fix: update response format in hello_world Lambda and adjust CORS settings in deployment_resources.json

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -3,10 +3,8 @@ exports.handler = async (event) => {
     // Create the response
     const response = {
         statusCode: 200,
-        body: JSON.stringify({
-            message: "Hello from Lambda"
-        }),
+        message: "Hello from Lambda",
     };
-    
+
     return response;
 };
