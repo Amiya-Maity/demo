033f7cce711c311f77014bb89d84e6330966e641
Amiya_Maity <amiyamaity7105@gmail.com>
2025-03-29 17:14:44 +0530

    refactor: clean up whitespace and improve logging in API handler

--- task05/app/lambdas/api_handler/index.js
+++ task05/app/lambdas/api_handler/index.js
@@ -1,14 +1,14 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { PutCommand } from "@aws-sdk/lib-dynamodb";
 import { v4 as uuidv4 } from "uuid";
- 
+
 const dynamoDBClient = new DynamoDBClient();
 const TABLE_NAME = process.env.TABLE_NAME || "Events";
- 
+
 export const handler = async (event) => {
     try {
         console.log("Received event:", JSON.stringify(event, null, 2));
- 
+        console.log("Event body:", event.body);
         let inputEvent;
         try {
             inputEvent = event.body;
@@ -19,36 +19,36 @@ export const handler = async (event) => {
                 body: JSON.stringify({ message: "Invalid JSON format in request body" })
             };
         }
- 
+
 
         const eventId = uuidv4();
         const createdAt = new Date().toISOString();
- 
+
         const eventItem = {
             id: eventId,
             principalId: Number(inputEvent.principalId),
             createdAt,
             body: inputEvent.content
         };
- 
+
         console.log("Saving to DynamoDB:", JSON.stringify(eventItem, null, 2));
- 
+
         const response = await dynamoDBClient.send(new PutCommand({
             TableName: TABLE_NAME,
             Item: eventItem,
         }));
         console.log("Saved successfully");
- 
+
         console.log("DynamoDB Response:", response);
- 
+
         const responseObject = {
             statusCode: 201,
-            body: JSON.stringify({statusCode : 201, event : eventItem})
+            body: JSON.stringify({ statusCode: 201, event: eventItem })
         };
- 
+
         console.log("Final response:", JSON.stringify(responseObject, null, 2));
         return responseObject;
- 
+
     } catch (error) {
         console.error("Error processing request:", error);
         return {
